90b20d0ad55de2c9f9135f0cbeffeee25305b8a1
pnikitos327-sys <owwwwkom@gmail.com>
2026-07-07 21:11:15 +0300

    change in admin.py

--- store/main/admin.py
+++ store/main/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Category, Product, ProductImage, Cart, CartItem, Order, OrderItem
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(ProductImage)
+admin.site.register(Cart)
+admin.site.register(CartItem)
+admin.site.register(Order)
+admin.site.register(OrderItem)
