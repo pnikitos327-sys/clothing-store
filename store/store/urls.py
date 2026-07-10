bf2c0907c266fd68f0b3ac6d005770ab8a2c91d2
pnikitos327-sys <owwwwkom@gmail.com>
2026-07-10 20:09:50 +0300

    new change

--- store/store/urls.py
+++ store/store/urls.py
@@ -1,7 +1,15 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
