b1e99d9878eb321b500338e56cc384cf8fc6a9c6
pnikitos327-sys <owwwwkom@gmail.com>
2026-07-11 19:11:16 +0300

    ...

--- store/main/views.py
+++ store/main/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render, redirect
+from .models import Product
 
 def home(request):
-    return render(request, 'index.html')
+    products = Product.objects.all()
+    context = {
+        'products': products
+    }
+    return render(request, 'index.html', context)
+
