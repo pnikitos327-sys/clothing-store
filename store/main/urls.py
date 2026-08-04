9fbc3d159e26627647349cde816e3805c2f6867a
pnikitos327-sys <owwwwkom@gmail.com>
2026-08-04 18:21:05 +0300

    registersions

--- store/main/urls.py
+++ store/main/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from .views import registersion_view
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', registersion_view, name='register'),
+    path('1', views.home, name='home'),
     path('cart/', views.cart_view, name='cart'),  
     path('category/<slug:slug>/', views.category_detail, name='category_detail'),
     path('add-to-cart/<int:product_id>/', views.add_to_cart, name='add_to_cart')
--- store/main/views.py
+++ store/main/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth import authenticate, login
 from .models import Product, Category, Cart, CartItem
+from .forms import RegistrationForm
+
+def registersion_view(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = RegistrationForm()
+    return render(request, 'register.html', {'form': form})
 
 def add_to_cart(request, product_id):
     product = get_object_or_404(Product, id=product_id)
@@ -35,4 +51,11 @@ def category_detail(request, slug):
     })
 
 def cart_view(request):
-    return render(request, 'cart.html')
+    if request.user.is_authenticated:
+        cart = Cart.objects.get(user=request.user)
+        items = CartItem.objects.filter(cart=cart)
+        total = sum(item.product.price * item.quantity for item in items)
+    else:
+        items = []
+        total = 0
+    return render(request, 'cart.html', {'items': items, 'total': total})
