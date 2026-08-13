60e7455ceb671ca3ee69beb748d172306699e323
ovvvll <ovvvll@Air-ovvvll.lan>
2026-08-13 10:41:06 -0700

    zaebalo bce

--- store/main/admin.py
+++ store/main/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from .models import Category, Product, ProductImage, Cart, CartItem, Order, OrderItem
 
+@admin.register(ProductImage)
+class ProductImageAdmin(admin.ModelAdmin):
+    list_filter = ['product']  
+
+
+
 admin.site.register(Category)
 admin.site.register(Product)
-admin.site.register(ProductImage)
 admin.site.register(Cart)
 admin.site.register(CartItem)
 admin.site.register(Order)
